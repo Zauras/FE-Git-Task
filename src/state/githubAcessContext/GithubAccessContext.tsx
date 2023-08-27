210df585d180a7d929d86ca7312cf67f8f9b90ba
Joris Medeisis <joris@nexchange.co.uk>
2023-08-27 20:49:34 +0300

    * add loading spinner to login

--- src/state/githubAcessContext/GithubAccessContext.tsx
+++ src/state/githubAcessContext/GithubAccessContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useState } from 'react';
-import { reqCheckIsGitHubAccessTokenValid, reqGetGitHubUser } from '@/api/gitHubApi';
+
+import { reqGetGitHubUser } from '@/api/gitHubApi';
 
 interface IGitHubAccessDto {
     accessToken: string;
@@ -10,38 +11,42 @@ const defaultGitHubAccessDto = {} as IGitHubAccessDto;
 
 const GitHubAccessContext = createContext<{
     gitHubAccessDto: IGitHubAccessDto;
-    logInByAccessToken: (accessToken: string) => void;
-    logOut: () => void;
+    isLoading: boolean;
+    logInByAccessToken?: (accessToken: string) => Promise<void>;
+    logOut?: () => void;
 }>({
     gitHubAccessDto: defaultGitHubAccessDto,
-    logInByAccessToken: (accessToken: string) => null,
-    logOut: () => null,
+    isLoading: false,
 });
 
 const GitHubAccessProvider = ({ children }: { children: ReactNode }) => {
+    const [isLoading, setIsLoading] = useState(false);
     const [gitHubAccessDto, setGitHubAccessDto] =
         useState<IGitHubAccessDto>(defaultGitHubAccessDto);
 
-    const handleSetAccessToken = (accessToken: string) => {
-        (async () => {
-            const { data, error } = await reqGetGitHubUser({ accessToken });
+    const handleSetAccessToken = async (accessToken: string) => {
+        setIsLoading(true);
+        const { data, error } = await reqGetGitHubUser({ accessToken });
 
-            if (data) {
-                setGitHubAccessDto({
-                    accessToken: accessToken,
-                    userName: data.name,
-                });
-            }
-        })();
+        if (data) {
+            setGitHubAccessDto({
+                accessToken: accessToken,
+                userName: data.name,
+            });
+        }
+        sessionStorage.setItem('gitHubAccessToken', accessToken);
+        setIsLoading(false);
     };
 
     const handleLogOut = () => {
         setGitHubAccessDto(defaultGitHubAccessDto);
+        sessionStorage.removeItem('gitHubAccessToken');
     };
 
     return (
         <GitHubAccessContext.Provider
             value={{
+                isLoading,
                 gitHubAccessDto,
                 logInByAccessToken: handleSetAccessToken,
                 logOut: handleLogOut,
